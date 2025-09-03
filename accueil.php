3e98f4bc71b6c83b5129159b6bf4291f505e4f8e
Jonh Doe <jonhdoe@example.com>
2025-09-03 15:24:22 +0200

    Mon projet version 1

---
+++ accueil.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head><body>
+<?php
+$prenom=$_POST["prenom"];
+$nom=$_POST["nom"];
+echo "<p> Bonjour $prenom $nom</p>";
+?>
+
