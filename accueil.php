605987add9e68fe3485f3a2db4ef33f6d1f10260
Yassin FATHALLAH <yfath@lmdsio.com>
2026-09-02 15:45:10 +0200

    Mon projet version 4

--- accueil.php
+++ accueil.php
@@ -12,7 +12,7 @@ $host = $_SERVER['HTTP_HOST']; // on récupère le nom de l’hôte
 $uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\'); // on récupère le début de l’URL
  // on redirige vers l’URL, en complétant les … par la nom de la page html
 if ($login == "admin" && $mdp == "azerty"){
-    header("Location: http://$host$uri/index.html");
+    header("Location: http://$host$uri/login.html");
 } else {
     header("Location: http://$host$uri/profil.html");
 }
